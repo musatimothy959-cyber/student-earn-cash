d42617e76f36b93fafab46de931714ba88a52851
musatimothy959-cyber <musatimothy959@gmail.com>
2026-06-30 13:30:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "student-earn-cash-d969a.firebaseapp.com",
+  projectId: "student-earn-cash-d969a",
+  storageBucket: "student-earn-cash-d969a.firebasestorage.app",
+  messagingSenderId: "788525383225",
+  appId: "1:788525383225:web:b850249f5f78876be5d7e8"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+console.log("Firebase connected!");
